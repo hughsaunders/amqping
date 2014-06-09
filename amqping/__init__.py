b727377c2458efe0f345505c471dc73c0c12e5eb
Hugh Saunders <hugh@wherenow.org>
2014-06-09 11:10:00 +0100

    add ability to pull a sample message, restructured into a class

--- amqping/__init__.py
+++ amqping/__init__.py
@@ -8,6 +8,45 @@ If an AMQP connection is established a success message is printed, otherwise an
 eception is printed.
 """
 
+class AMQPing:
+
+    def __init__(self, opts):
+        self.opts = opts
+
+    def amqp_connect(self):
+        credentials = pika.PlainCredentials(self.opts.user, self.opts.password)
+        parameters = pika.ConnectionParameters(credentials=credentials,
+                                               virtual_host="/%s"%self.opts.vhost,
+                                               host=self.opts.host,
+                                               port=self.opts.port)
+        self.connection = pika.SelectConnection(parameters, self.on_connected)
+        try:
+            # Loop so we can communicate with RabbitMQ
+            self.connection.ioloop.start()
+        except KeyboardInterrupt:
+            # Gracefully close the connection
+            self.connection.close()
+            # Loop until we're fully closed, will stop on its own
+            self.connection.ioloop.start()
+
+
+    def on_connected(self, connection):
+        """ Connection callback """
+        print "Successfully opened AMQP connection."
+        if self.opts.queue:
+            channel = self.connection.channel(self.on_open)
+        else:
+            connection.close()
+
+    def on_open(self, channel):
+        print "Channel open"
+        self.channel = channel
+        self.channel.basic_get(self.on_recv, self.opts.queue)
+
+    def on_recv(self, *args):
+        print "Received Message: ",args[3]
+        self.connection.close()
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--user')
@@ -15,30 +54,11 @@ def main():
     parser.add_argument('-v', '--vhost', default='', help="Defaults to /")
     parser.add_argument('-H', '--host', default="localhost", help="Defaults to localhost")
     parser.add_argument('-P', '--port', default=5672, type=int, help="Defaults to 5672")
+    parser.add_argument('-Q', '--queue', type=str, help="Get a sample message form this"
+                "queue")
     options = parser.parse_args()
-    amqp_connect(options.user, options.password, options.vhost, options.host, options.port)
-
-def amqp_connect(user,password,vhost,host,port):
-    credentials = pika.PlainCredentials(user, password)
-    parameters = pika.ConnectionParameters(credentials=credentials,
-                                           virtual_host="/%s"%vhost,
-                                           host=host,
-                                           port=port)
-    connection = pika.SelectConnection(parameters, on_connected)
-    try:
-        # Loop so we can communicate with RabbitMQ
-        connection.ioloop.start()
-    except KeyboardInterrupt:
-        # Gracefully close the connection
-        connection.close()
-        # Loop until we're fully closed, will stop on its own
-        connection.ioloop.start()
-
-
-def on_connected(connection):
-    """ Connection callback """
-    print "Successfully opened AMQP connection."
-    connection.close()
+    amqping = AMQPing(options)
+    amqping.amqp_connect()
 
 if __name__ == "__main__":
     main()
