c261bdb8c1c58ecc0dbccc0c18ba11270e79be6e
Hugh Saunders <hugh@wherenow.org>
2014-06-09 11:11:34 +0100

    bump setup.py version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
 setup(name='amqping',
-      version=0.1,
+      version=0.2,
       description=('simple package/command for tesitng if an amqp instance is'
                    ' responding'),
       author='Hugh Saunders',
       author_email='hugh@wherenow.org',
       license='MIT',
       packages=find_packages(),
-      keywords="amqp rabbit ping",
+      keywords="amqp rabbitmq ping",
       url="https://github.com/hughsaunders/amqping",
       entry_points={
           'console_scripts': [
